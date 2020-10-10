1c1deac06ca5bd322985b9e6c9b23feb688a0c1d
Rudolf Achter <rudolf.achter@megatech-communication.de>
2020-10-10 07:15:56 +0200

    update: Entfernen von Script die nicht offiziell zu den Modulen gehören

--- Config/Netbox-Integration.config.ps1
+++
@@ -1,6 +0,0 @@
-
-$Global:Netbox = @{
-    ApiUrl="https://netbox.zim.uni-passau.de/api"
-    #Get your Own Token from Netbox
-    ApiToken="XXXXXXXXXX"
-}
--- Scripts/QuestCMGLogsPlayground.ps1
+++
@@ -1,3 +0,0 @@
-﻿New-SmbMapping -LocalPath T: -RemotePath "\\quest-cmg\c$" -UserName msxadmin@ads.uni-passau.de -Password "M8msNl0s!adS"
-
-Get-QuestCMGProcessedMessages | Export-Csv -Path "H:\Todo\2020-08-14_QuestCMGLogs\2020-09-07_QuestCMGProcessedMessages.csv" -NoTypeInformation
